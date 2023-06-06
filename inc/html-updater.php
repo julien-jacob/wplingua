7b4846ff0ab432278905de03831f1803de0a3ea2
Julien MA Jacob <juulien.jacob@gmail.com>
2023-06-06 01:27:01 -0200

    Format PHP

--- inc/html-updater.php
+++ inc/html-updater.php
@@ -1,14 +1,13 @@
 <?php
 
 // If this file is called directly, abort.
-if (!defined('WPINC')) {
+if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-function mcv_replace_og_local($html)
-{
+function mcv_replace_og_local( $html ) {
 
-	if (!mcv_url_current_is_translatable()) {
+	if ( ! mcv_url_current_is_translatable() ) {
 		return $html;
 	}
 
@@ -22,18 +21,17 @@ function mcv_replace_og_local($html)
 }
 
 
-function mcv_language_attributes($attr)
-{
+function mcv_language_attributes( $attr ) {
 
 	$language_current_id = mcv_get_language_current_id();
 
-	if (is_admin() || empty($language_current_id)) {
+	if ( is_admin() || empty( $language_current_id ) ) {
 		return $attr;
 	}
 
 	$attr = preg_replace(
 		'#lang=(\"|\')(..)-(..)(\"|\')#i',
-		'lang=$1' . esc_attr($language_current_id) . '$4',
+		'lang=$1' . esc_attr( $language_current_id ) . '$4',
 		$attr
 	);
 
@@ -42,20 +40,19 @@ function mcv_language_attributes($attr)
 
 
 
-function mcv_link_alternate_hreflang()
-{
+function mcv_link_alternate_hreflang() {
 
 	$html = '';
 
 	// Create alternate link for website language
 	$language_website = mcv_get_language_website();
-	$html            .= '<link rel="alternate" hreflang="' . esc_attr($language_website['id']) . '" href="' . esc_url(mcv_get_url_original()) . '">';
+	$html            .= '<link rel="alternate" hreflang="' . esc_attr( $language_website['id'] ) . '" href="' . esc_url( mcv_get_url_original() ) . '">';
 
 	// Create alternate link for each target languages
 	$languages_target = mcv_get_languages_target();
-	foreach ($languages_target as $key => $language_target) {
-		$url   = mcv_get_url_current_for_language($language_target['id']);
-		$html .= '<link rel="alternate" hreflang="' . esc_attr($language_target['id']) . '" href="' . esc_url($url) . '">';
+	foreach ( $languages_target as $key => $language_target ) {
+		$url   = mcv_get_url_current_for_language( $language_target['id'] );
+		$html .= '<link rel="alternate" hreflang="' . esc_attr( $language_target['id'] ) . '" href="' . esc_url( $url ) . '">';
 	}
 
 	echo $html;
@@ -63,20 +60,19 @@ function mcv_link_alternate_hreflang()
 
 
 
-function mcv_init()
-{
+function mcv_init() {
 
-	if (!mcv_url_current_is_translatable()) {
+	if ( ! mcv_url_current_is_translatable() ) {
 		return;
 	}
 
 	global $mcv_request_uri;
 
 	$current_path           = $mcv_request_uri;
-	$origin_path            = '/' . substr($current_path, 4, strlen($current_path) - 1);
+	$origin_path            = '/' . substr( $current_path, 4, strlen( $current_path ) - 1 );
 	$_SERVER['REQUEST_URI'] = $origin_path;
 
-	ob_start('mcv_ob_callback');
+	ob_start( 'mcv_ob_callback' );
 }
 
 
@@ -84,9 +80,7 @@ function mcv_init()
 
 
 
-function mcv_ob_callback($html)
-{
-
+function mcv_ob_callback( $html ) {
 
 	$selector_clear = array(
 		'style',
@@ -103,62 +97,56 @@ function mcv_ob_callback($html)
 	 */
 
 	// Remove comments from HTML Clear
-	$html_clear = preg_replace('#<!--.*-->#Uis', '', $html);
-
-
+	$html_clear = preg_replace( '#<!--.*-->#Uis', '', $html );
 
 	// Remove useless and excluded elements from HTML clear
-	$dom = str_get_html($html_clear);
+	$dom = str_get_html( $html_clear );
 
-	if ($dom === false) {
+	if ( $dom === false ) {
 		return $html;
 	}
 
-	$selector_to_remove = array_merge($selector_exclude, $selector_clear);
+	$selector_to_remove = array_merge( $selector_exclude, $selector_clear );
 
-	foreach ($selector_to_remove as $key => $selector) {
-		foreach ($dom->find($selector) as $element) {
+	foreach ( $selector_to_remove as $key => $selector ) {
+		foreach ( $dom->find( $selector ) as $element ) {
 			$element->outertext = '';
 		}
 	}
 
 	$dom->save();
-	$html_clear = (string) str_get_html($dom);
-
-
+	$html_clear = (string) str_get_html( $dom );
 
 	/**
 	 * Get saved translation
 	 */
 	$mcv_language_target = mcv_get_language_current_id();
-	$translations = mcv_get_saved_translations($mcv_language_target);
+	$translations        = mcv_get_saved_translations( $mcv_language_target );
 	// return '<pre >' . var_export($html_clear, true) . '</pre>';
-
 
 	/**
 	 * Remove saved translation from HTML clear
 	 */
 	// Clear HTML of know translation
-	foreach ($translations as $translation) {
+	foreach ( $translations as $translation ) {
 
 		// Check if translaton data is valid
 		if (
-			!isset($translation['source']) // Original text
-			|| !isset($translation['translation']) // Translater text
-			|| !isset($translation['search']) // Search
-			|| !isset($translation['replace']) // Replace
+			! isset( $translation['source'] ) // Original text
+			|| ! isset( $translation['translation'] ) // Translater text
+			|| ! isset( $translation['search'] ) // Search
+			|| ! isset( $translation['replace'] ) // Replace
 		) {
 			continue;
 		}
 
 		$regex = str_replace(
 			'MCV',
-			preg_quote($translation['source']),
+			preg_quote( $translation['source'] ),
 			// '#>(\s*)MCV(\s*)<#Uis'
 			$translation['search']
 		);
 		// Replace knowing translation by empty string
-
 
 		$html_clear = preg_replace(
 			$regex,
@@ -175,57 +163,47 @@ function mcv_ob_callback($html)
 	/**
 	 * Get new translation from API
 	 */
-	$translations_new = mcv_parser($html_clear);
-
+	$translations_new = mcv_parser( $html_clear );
 
 	// Merge know and new translations
-	$translations = array_merge($translations, $translations_new);
-
-
-
-
-
+	$translations = array_merge( $translations, $translations_new );
 
 	$excluded_elements = array();
-	$dom = str_get_html($html);
+	$dom               = str_get_html( $html );
 
-	if ($dom === false) {
+	if ( $dom === false ) {
 		return $html;
 	}
 
-	foreach ($selector_exclude as $key => $selector) {
-		foreach ($dom->find($selector) as $element) {
+	foreach ( $selector_exclude as $key => $selector ) {
+		foreach ( $dom->find( $selector ) as $element ) {
 			$excluded_elements[] = $element->outertext;
-			$attr = count($excluded_elements) - 1;
-			$element->outertext = '<div mcv-tag-exclude="' . esc_attr($attr) . '"></div>';
+			$attr                = count( $excluded_elements ) - 1;
+			$element->outertext  = '<div mcv-tag-exclude="' . esc_attr( $attr ) . '"></div>';
 		}
 	}
 
 	$dom->save();
-	$html = (string) str_get_html($dom);
-
-
-
-
+	$html = (string) str_get_html( $dom );
 
 	// Replace original texts by translations
-	foreach ($translations as $translation) {
+	foreach ( $translations as $translation ) {
 
 		// Check if translaton data is valid
 		if (
-			!isset($translation['source']) // Original text
-			|| !isset($translation['translation']) // Translater text
-			|| !isset($translation['search']) // Search
-			|| !isset($translation['replace']) // Replace
+			! isset( $translation['source'] ) // Original text
+			|| ! isset( $translation['translation'] ) // Translater text
+			|| ! isset( $translation['search'] ) // Search
+			|| ! isset( $translation['replace'] ) // Replace
 		) {
 			continue;
 		}
 
-		if (!empty($translation['source'])) {
+		if ( ! empty( $translation['source'] ) ) {
 
 			$regex = str_replace(
 				'MCV',
-				preg_quote($translation['source']),
+				preg_quote( $translation['source'] ),
 				$translation['search']
 			);
 
@@ -235,23 +213,21 @@ function mcv_ob_callback($html)
 				$translation['replace']
 			);
 
-
 			// Replace original text in HTML by translation
-			$html = preg_replace($regex, $replace, $html);
+			$html = preg_replace( $regex, $replace, $html );
 		}
 	}
 
-
 	// Save new translation file
-	if (!empty($translations_new)) {
+	if ( ! empty( $translations_new ) ) {
 
-		foreach ($translations_new as $key => $translation) {
+		foreach ( $translations_new as $key => $translation ) {
 
 			if (
-				!isset($translation['source']) // Original text
-				|| !isset($translation['translation']) // Translater text
-				|| !isset($translation['search']) // Search
-				|| !isset($translation['replace']) // Replace
+				! isset( $translation['source'] ) // Original text
+				|| ! isset( $translation['translation'] ) // Translater text
+				|| ! isset( $translation['search'] ) // Search
+				|| ! isset( $translation['replace'] ) // Replace
 			) {
 				continue;
 			}
@@ -268,16 +244,12 @@ function mcv_ob_callback($html)
 		}
 	}
 
-
-
-
-	foreach ($excluded_elements as $key => $element) {
-		$s = '<div mcv-tag-exclude="' . esc_attr($key) . '"></div>';
-		$html = str_replace($s, $element, $html);
+	foreach ( $excluded_elements as $key => $element ) {
+		$s    = '<div mcv-tag-exclude="' . esc_attr( $key ) . '"></div>';
+		$html = str_replace( $s, $element, $html );
 	}
 
-
-	$html = apply_filters('mcv_html_translated', $html);
+	$html = apply_filters( 'mcv_html_translated', $html );
 
 	return $html;
 }
