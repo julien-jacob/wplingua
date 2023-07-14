5757b749c890b6785b55cc40274ba9b4d0424a84
Julien MA Jacob <juulien.jacob@gmail.com>
2023-07-14 06:04:27 -0200

    Switcher shortcode look working

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -10,33 +10,22 @@ if ( ! defined( 'WPINC' ) ) {
 
 // TODO : Shortcode switcher
 function wplng_shortcode_switcher( $atts ) {
-	
+
 	$attributes = shortcode_atts(
 		array(
-			'style' => false,
-			'theme' => false,
-			'title' => false,
+			'insert' => false,
+			'style'  => false,
+			'title'  => false,
+			'theme'  => false,
+			'flags'  => false,
+			'class'  => false,
 		), $atts
 	);
 
 	$class = 'insert-shortcode';
 
-	if ( empty( $attributes['style'] ) ) {
-		$class .= ' style-' . wplng_get_switcher_style();
-	}
 
-	if ( empty( $attributes['theme'] ) ) {
-		$class .= ' theme-' . wplng_get_switcher_theme();
-	}
-
-	if ( empty( $attributes['title'] ) ) {
-		$class .= ' title-' . wplng_get_switcher_name_format();
-	}
-
-	return wplng_get_switcher_html(
-		$class,
-		wplng_get_switcher_flags_style() !== 'none'
-	);
+	return wplng_get_switcher_html( $attributes );
 
 }
 add_shortcode( 'wplingua-switcher', 'wplng_shortcode_switcher' );
