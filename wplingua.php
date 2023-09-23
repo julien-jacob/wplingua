0119fe0b96de69b337965c55bac3973cc048dc2b
Julien MA Jacob <juulien.jacob@gmail.com>
2023-09-23 06:42:03 -0200

    Update version number

--- wplingua.php
+++ wplingua.php
@@ -7,7 +7,7 @@
  * Author URI:   https://wplingua.com/
  * Text Domain:  wplingua
  * Domain Path:  /languages/
- * Version:      0.1.0
+ * Version:      0.1.1
  * Requires PHP: 7.0
  */
 
@@ -23,7 +23,7 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'WPLNG_API_URL', 'https://api.wplingua.com' );
 define( 'WPLNG_API_VERSION', '1.0' );
-define( 'WPLNG_PLUGIN_VERSION', '0.1.0' );
+define( 'WPLNG_PLUGIN_VERSION', '0.1.1' );
 define( 'WPLNG_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'WPLNG_MAX_TRANSLATIONS', 256 );
 define( 'WPLNG_MAX_FILE_SIZE', 1000000 );
@@ -111,6 +111,7 @@ function wplng_start() {
 		// Clear translation cache on trash / untrash
 		add_action('trashed_post', 'wplng_clear_translations_cache_trash_untrash'); 
 		add_action('untrash_post', 'wplng_clear_translations_cache_trash_untrash'); 
+		add_action('delete_post', 'wplng_clear_translations_cache_trash_untrash'); 
 
 		// Enqueue Script for wplng_translation admin
 		add_action( 'admin_print_scripts-post-new.php', 'wplng_translation_assets' );
