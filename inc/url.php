3ef252611e4c60ea6843bd0d19f3b4e7d3a197fc
Julien MA Jacob <juulien.jacob@gmail.com>
2024-10-12 01:57:31 -0200

    Fix slug translated redirect: fragment / query URL

--- inc/url.php
+++ inc/url.php
@@ -84,7 +84,9 @@ function wplng_url_translate( $url, $language_target_id = '' ) {
 			$url
 		);
 
-		if ( empty( $parsed_url['fragment'] ) ) {
+		if ( empty( $parsed_url['fragment'] ) 
+			&& empty( $parsed_url['query'] )
+		) {
 			// Add slash at the end if is not an anchor link
 			$url = trailingslashit( $url );
 		}
