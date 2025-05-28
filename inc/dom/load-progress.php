8a83caefd3d3ae4c691e3eadc946bb27a639c89a
Julien MA Jacob <juulien.jacob@gmail.com>
2025-05-28 07:17:55 -0200

    Review Load in progress for AJAX

--- inc/dom/load-progress.php
+++ inc/dom/load-progress.php
@@ -58,9 +58,9 @@ function wplng_dom_load_progress( $dom, $args ) {
 
 		if ( ! wplng_get_api_overloaded() ) {
 
-			$redirect_query_arg['wplng-load']    = 'progress';
-			$redirect_query_arg['nocache'] = (string) time() . (string) rand( 100, 999 );
-			$redirect_needed                     = true;
+			$redirect_query_arg['wplng-load'] = 'progress';
+			$redirect_query_arg['nocache']    = (string) time() . (string) rand( 100, 999 );
+			$redirect_needed                  = true;
 		}
 
 		if ( $redirect_needed ) {
@@ -188,17 +188,30 @@ function wplng_dom_load_progress( $dom, $args ) {
 
 		$url_reload = add_query_arg(
 			array(
-				'wplng-load'    => 'progress',
-				'nocache' => (string) time() . (string) rand( 100, 999 ),
+				'wplng-load' => 'progress',
+				'nocache'    => (string) time() . (string) rand( 100, 999 ),
 			),
 			$url_reload
 		);
 
 	}
 
+	// Make the URL to load
+
+	$url_load = add_query_arg(
+		array(
+			'wplng-load' => 'loading',
+			'nocache'    => (string) time() . (string) rand( 100, 999 ),
+		),
+		$args['url_current']
+	);
+
+	// Make the HTML
+
 	$html  = '<div';
 	$html .= ' id="wplng-in-progress-container"';
 	$html .= ' wplng-reload="' . esc_url( $url_reload ) . '"';
+	$html .= ' wplng-load="' . esc_url( $url_load ) . '"';
 	$html .= '>';
 
 	$html .= '<div id="wplng-in-progress-message">';
@@ -227,27 +240,8 @@ function wplng_dom_load_progress( $dom, $args ) {
 	$html .= '</div>'; // End #wplng-progress-bar-value
 	$html .= '</div>'; // End #wplng-progress-bar
 
-	$html .= '</div>'; // End #wplng-in-progress-container
+	$html .= '</div>'; // End #wplng-in-progress-containe
 
-	/**
-	 * Create the html of iframe
-	 */
-
-	$url_iframe = add_query_arg(
-		array(
-			'wplng-load'    => 'loading',
-			'nocache' => (string) time() . (string) rand( 100, 999 ),
-		),
-		$args['url_current']
-	);
-
-	$html .= '<iframe';
-	$html .= ' id="wplng-in-progress-iframe"';
-	$html .= ' src="' . esc_url( $url_iframe ) . '"';
-	$html .= ' style="display: none !important;"';
-	$html .= '>';
-	$html .= '</iframe>'; // End #wplng-translation-in-progress
-	
 	/**
 	 * Place the HTML in the end of body
 	 */
